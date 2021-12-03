ff764f299e59fe44b01e388d9013f93ec974d337
Mati <matildetozzi@gmail.com>
2021-12-03 19:20:53 +0100

    syntax improvement aoc_3_2

--- Day3/aoc_3_2.py
+++ Day3/aoc_3_2.py
@@ -26,12 +26,9 @@ for i in range (0, len(ox_lines[0])-1):
 	# select the values according to the most common value
 	if one_occ >= zero_occ:
 		for line in ox_lines:
-			if line[i] == "1":
-				ox_list.append(line)
+			ox_list = [line for line in ox_lines if line[i] == '1']
 	else:
-		for line in ox_lines:
-			if line[i] == "0":
-				ox_list.append(line)
+		ox_list = [line for line in ox_lines if line[i] == '0']
 	ox_lines = ox_list
 	# check if a single value is reached
 	if len(ox_list) == 1:
@@ -51,13 +48,9 @@ for i in range (0, len(co2_lines[0])-1):
 			zero_occ = zero_occ + 1
 	# select the values according to the least common value
 	if zero_occ <= one_occ:
-		for line in co2_lines:
-			if line[i] == "0":
-				co2_list.append(line)
+		co2_list = [line for line in co2_lines if line[i] == '0']
 	else:
-		for line in co2_lines:
-			if line[i] == "1":
-				co2_list.append(line)
+		co2_list = [line for line in co2_lines if line[i] == '1']
 	co2_lines = co2_list
 	# check if a single value is reached
 	if len(co2_list) == 1:
