f8d8592beebfd5db616654557fd88e1f4cf57981
Mati <matildetozzi@gmail.com>
2021-12-08 11:44:17 +0100

    day 8

---
+++ Day8/aoc_8_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 10:31:10 2021
+
+@author: mati
+"""
+count = 0
+with open('input.txt', 'r') as f:
+	while True:
+		parts = f.readline().split('|')
+		if len(parts)!=2:
+			break
+		digits = parts[1].split()
+		for digit in digits:
+			if len(digit) == 2 or len(digit) == 4 or len(digit) == 3\
+				or len(digit) == 7:
+					count = count+1
+print(count)
---
+++ Day8/aoc_8_2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 10:40:30 2021
+
+@author: mati
+"""
+
+total = 0
+with open('input.txt', 'r') as f:
+	while True:
+		parts = f.readline().split('|')
+		if len(parts)!=2:
+			break
+		examples = parts[0].split()
+		one = set([example for example in examples if len(example)==2][0])
+		seven = set([example for example in examples if len(example)==3][0])
+		four = set([example for example in examples if len(example)==4][0])
+		eight = set([example for example in examples if len(example)==7][0])
+		lower_left = eight.copy()
+		for el in (four.union(seven)):
+			lower_left.discard(el)
+		digits = parts[1].split()
+		i = 0
+		output = [0,0,0,0]
+		for digit in digits:
+			digit = set(digit)
+			if len(digit) == 2:
+				output[i] = 1
+			elif len(digit) == 3:
+				output[i] = 7
+			elif len(digit) == 4:
+				output[i] = 4
+			elif len(digit) == 7:
+				output[i] = 8
+			elif len(digit) == 6:
+				if four < digit:
+					output[i] = 9
+				elif seven < digit:
+					output[i] = 0
+				else:
+					output[i] = 6
+			else:
+				if seven < digit:
+					output[i] = 3
+				elif lower_left < digit:
+					output[i] = 2
+				else:
+					output[i] = 5
+			i = i+1
+		output = output[3]+10*output[2]+100*output[1]+1000*output[0]
+		total = total + output
+print(total)
